404bf4eacb6f0e71a1593a76362626cde0d58b7e
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-04-26 11:50:56 +0200

    Battle Areas

---
+++ 2dPlattformer/Assets/Camera/Arena.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arena : MonoBehaviour {
+    public CameraFollow cam;
+    private CameraFollow originalCamera;
+    //private Vector3 Position;
+    public GameObject lazer;
+    public GameObject exitLazer;
+    public int killcount=1;
+    public int arenaCameraSize = 10;
+    public float sizeSpeed = 0.1f;
+
+
+    private void Update()
+    {
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.CompareTag("Player"))
+        {Debug.Log("Enter");
+
+            cam.enterdArena = true;
+            cam.arenaCameraSize = arenaCameraSize;
+            cam.sizeSpeed = sizeSpeed;
+            originalCamera = cam;
+
+            lazer.SetActive(true);
+        }
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            cam._targetPosition = transform.Find("CameraPosition").position;
+
+            //cam.orthographicSize = cameraSize;
+
+            // Spelaren har triggat cameratrigger
+            //cam._targetPosition = new Vector3(cam._targetPosition.x, cam._targetPosition.y, camDepth);
+        }
+        if(killcount==0){
+           exitLazer.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       
+        if (collision.CompareTag("Player"))
+        { Debug.Log("exit");
+            // Spelaren har triggat cameratrigger
+            cam.enterdArena = false;
+            //cam._targetPosition = new Vector3(cam._targetPosition.x, cam._targetPosition.y, cam._targetPosition.z);
+        }
+    }
+}
--- 2dPlattformer/Assets/Camera/CameraFollow.cs
+++ 2dPlattformer/Assets/Camera/CameraFollow.cs
@@ -6,46 +6,109 @@ public class CameraFollow : MonoBehaviour
 {
     public PlayerController Player;
     public Vector3 Offset;
-    private Vector3 _targetPosition;    [Header("Follow")]
+    public Vector3 _targetPosition;
+    public Camera _orthographicCamera;
+
+    [Header("Follow")]
     public float SmoothingTime;
-    private Vector3 _currentVelocity;    [Header("Look ahead")]
+    private Vector3 _currentVelocity;
+
+    [Header("Look ahead")]
     public float MaxLookAhead;
     public float LookAheadAccelerationTime;
-    private float _lookAheadSpeed;    private float _lookAhead;    [Header("Look around")]
+    private float _lookAheadSpeed;
+    private float _lookAhead;
+
+    [Header("Look around")]
     public float MaxLookAroundAmount;
     public float TimeBeforeLookAround;
     public float PlayerMaximumSpeedForLookAround;
     private float _playerStillTime;
-    private float _lookAroundAmount;    public void Update()
+    private float _lookAroundAmount;
+
+    [Header("BattelArena")]
+    public bool enterdArena = false;
+    public int arenaCameraSize = 10;
+    public int regularCameraSize = 5;
+    public float sizeSpeed = 0.1f;
+    private float size;
+
+    public void Start()
     {
-        UpdateTargetPosition();
+        size = regularCameraSize;
+    }
+
+    public void Update()
+    {
+        if (!enterdArena)
+        {
+            //Debug.Log("Innan Lerp: " + arenaCameraSize + " ärvärde: " + regularCameraSize);
+
+            //size = Mathf.Lerp(size, regularCameraSize, t);
+            //Debug.Log(Mathf.Lerp(size, regularCameraSize, sizeSpeed));
+
+            //cameraSize = regularCameraSize;
+            //uppdateSize();
+            UpdateTargetPosition();
+            if (size > regularCameraSize)
+            {
+                size = size - sizeSpeed;
+            }
+
+        }
+        else
+        {
+            if (size < arenaCameraSize)
+            {
+                size = size + sizeSpeed;
+            }
+            //size = Mathf.Lerp(size, arenaCameraSize, t );
+            // cameraSize = arenaCameraSize;
+            // uppdateSize();
+        }
+        //Debug.Log(size);
+        _orthographicCamera.orthographicSize = size;
+
+
+
     }
 
     private void LateUpdate()
     {
-   
         UpdateMovement();
-        UpdateLookAhead();
-        UpdateLookAround();
+        if (!enterdArena)
+        {
 
-    }    private void UpdateTargetPosition()
+            UpdateLookAhead();
+            UpdateLookAround();
+        }
+
+
+    }
+
+    private void UpdateTargetPosition()
     {
         _targetPosition = Player.transform.position;
         _targetPosition += Offset;
         _targetPosition += Vector3.right * _lookAhead;
-        _targetPosition += Vector3.up * _lookAroundAmount;
+        _targetPosition += Vector3.up * _lookAroundAmount;
+
     }
 
 
     private void UpdateMovement()
     {
         transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _currentVelocity, SmoothingTime);
-    }    private void UpdateLookAhead()
+    }
+
+    private void UpdateLookAhead()
     {
         float targetLookAhead = MathHelper.Sign(Player.Velocity.x) * MaxLookAhead;
         _lookAhead = Mathf.SmoothDamp(_lookAhead, targetLookAhead,
         ref _lookAheadSpeed, LookAheadAccelerationTime);
-    }    private void UpdateLookAround()
+    }
+
+    private void UpdateLookAround()
     {
         if (Player.Velocity.magnitude > PlayerMaximumSpeedForLookAround)
         {
@@ -56,5 +119,7 @@ public class CameraFollow : MonoBehaviour
         _playerStillTime += Time.deltaTime;
         if (_playerStillTime < TimeBeforeLookAround) return;
         _lookAroundAmount = Input.GetAxisRaw("Vertical") * MaxLookAroundAmount;
-    }
+    }
+
+
 }
