f677133de44e606b35098a4a9e0a439bbf5965da
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-05-04 14:08:23 +0200

    WORKNOW

---
+++ 2dPlattformer/Assets/Scripts/Enemies/EnemyManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+    public int health;
+
+	public bool spawnEnemy = false;
+
+    /*public GameObject shield;
+    public int aktivateShield;
+    public GameObject weekPoint;
+    public int curentShieldHealth;
+    public int shieldHealth;
+	
+    public float timer;
+    public float startTimer;
+	*/
+
+    // Use this for initialization
+    void Start () {
+        //timer = 0;
+		//plasera fiende i lista om inte en spawnEnemy
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (health == 0)
+        {
+			if (spawnEnemy)
+				Destroy (gameObject);
+			else if (!spawnEnemy)
+				gameObject.SetActive (false);
+				
+        }
+        /*if (health<=aktivateShield&&shield!=null)
+        {
+            //Debug.Log(curentShieldHealth+" "+timer);
+            if (timer <= 0)
+            {
+                shield.SetActive(true);
+                weekPoint.SetActive(true);
+                timer = startTimer;
+                curentShieldHealth = shieldHealth ;
+            }
+
+            if (curentShieldHealth==0)
+            {
+                
+                shield.SetActive(false);
+                weekPoint.SetActive(false);
+                timer -= Time.deltaTime;
+
+            }
+		}*/
+	}
+}
---
+++ 2dPlattformer/Assets/Scripts/Enemies/SwithPatrolPoint.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwithPatrolPoint : MonoBehaviour {
+
+	public GameObject parentEnemy;
+
+	public Transform[] stage1PatrolPoints;
+	public Transform[] stage2PatrolPoints;
+
+	public int healthSwitch; 
+	public int health;
+
+	public bool aktivatedStage=false;
+
+	//public float timer;
+	//public float startTimer;
+
+	// Use this for initialization
+	void Start () {
+		//timer = startTimer;
+		parentEnemy.GetComponent<DronePatrolState> ().patrolPoints = stage1PatrolPoints;
+		aktivatedStage=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health = parentEnemy.GetComponent<EnemyManager> ().health;
+		Debug.Log(parentEnemy.GetComponent<EnemyManager>().health+" "+healthSwitch+" "+aktivatedStage);
+		//timer -= Time.deltaTime;
+		if(parentEnemy.GetComponent<EnemyManager>().health==healthSwitch&&!aktivatedStage){
+		//if(timer<=0){
+			//healthSwitch+=10;
+			aktivatedStage=true;
+		
+			if (parentEnemy.GetComponent<DronePatrolState> ().patrolPoints == stage1PatrolPoints) {
+				parentEnemy.GetComponent<DronePatrolState> ().patrolPoints = stage2PatrolPoints;
+				parentEnemy.GetComponent<ShieldManager> ().state = 1;
+			}
+			else if(parentEnemy.GetComponent<DronePatrolState> ().patrolPoints == stage2PatrolPoints)
+				parentEnemy.GetComponent<DronePatrolState> ().patrolPoints = stage1PatrolPoints;
+			//timer = startTimer;
+		}
+		//}
+
+	}
+}
